4ee4e2b0f4a1202e9941ea786c423d45defb9271
Md. Nazmul Alam <nazmulalamtuhin11011@gmail.com>
2025-01-15 19:15:24 +0600

    gitignore update 1

--- Main.java
+++ Main.java
@@ -1,16 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        printDayOfWeek(0);
-    }
-    public static void printDayOfWeek(int day) {
-        String dayOfWeek = switch (day) {
-            case 0 -> "Sunday";
-            case 1 -> "Monday";
-            case 2 -> "Tuesday";
-            case 3 -> "Wednesday";
-            default -> "Invalid";
-        };
-
-        System.out.println(day+" stands for "+dayOfWeek);
+        for(int counter=0;counter<5;counter++){
+            System.out.println(counter);
+        }
     }
 }
